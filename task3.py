dfb018ee47deae4855efbafea6c4ea53fa648165
ejones001 <ejonesnavy@gmail.com>
2024-05-06 22:03:15 -0500

    Task 3

---
+++ task3.py
@@ -0,0 +1,8 @@
+# Prompting the user to input a year
+year = int(input("Enter a year: "))
+
+# Checking if the year is a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
