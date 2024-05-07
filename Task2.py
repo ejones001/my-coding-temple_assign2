5c54c655dc2c3a0b6fd7cb0d1e6c7281e7ac182e
ejones001 <ejonesnavy@gmail.com>
2024-05-06 21:46:40 -0500

    Task 2

---
+++ Task2.py
@@ -0,0 +1,15 @@
+# Prompting the user to enter three numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Identifying the largest number among the three
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Printing the largest number
+print("The largest number is:", largest)
